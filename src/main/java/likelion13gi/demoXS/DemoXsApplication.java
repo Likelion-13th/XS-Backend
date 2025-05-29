1f7f302b2d3383b57f1237f32b41572bed72f970
EarlyXS <shm02156@gmail.com>
2025-05-29 19:55:55 +0900

    first message

---
+++ src/main/java/likelion13gi/demoXS/DemoXsApplication.java
@@ -0,0 +1,13 @@
+package likelion13gi.demoXS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoXsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoXsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/likelion13gi/demoXS/DemoXsApplicationTests.java
@@ -0,0 +1,13 @@
+package likelion13gi.demoXS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoXsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
